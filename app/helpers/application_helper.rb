de91964d7c5c14058edfd9ea8d07ff585c280cf7
Douglas Lima <Douglas@.(none)>
2010-02-13 06:25:38 -0400

    Just a comment test commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,3 @@
-# Methods added to this helper will be available to all templates in the application.
+# Methods added to this helper will be available to all templates in the application. test
 module ApplicationHelper
 end
